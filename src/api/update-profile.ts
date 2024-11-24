c8ee4d752f48b5bd0746b5aa3eaa808aa563fd3e
jemluz <contatojemima@gmail.com>
2024-11-24 11:50:07 -0300

    feat: add otmist interface feature for update profile

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -6,5 +6,10 @@ interface UpdateProfileBody {
 }
 
 export async function updateProfile({ description, name }: UpdateProfileBody) {
+  // use this to test otmist interface
+  // await new Promise((_, reject) => {
+  //   setTimeout(reject, 1000)
+  // })
+
   await api.put('/profile', { name, description })
 }
--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -23,9 +23,29 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 
+// this code is responsible for update the cached name at header
+// when update profile success= header name will updated too
+// onSuccess receive the returned data , and request data
+// first param -> data = the data received as res, unusefful in this case, so we write an "_"
+// second param -> variables = the data that we send on req
+// onSuccess(_, { description, name }) { // onSuccess = run when end the req with success
+//   const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+//     'managed-restaurant',
+//   ])
+//   if (cached) {
+//     queryClient.setQueryData<GetManagedRestaurantResponse>(
+//       ['managed-restaurant'],
+//       {
+//         ...cached,
+//         name,
+//         description,
+//       },
+//     )
+//   }
+
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
+  description: z.string().nullable(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -55,25 +75,60 @@ export function StoreProfileDialog() {
     },
   })
 
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
+
+    // if already has some cached data
+    if (cached) {
+      // update the chache with new values
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
+    }
+
+    // when use at onSuccess -> dont need to has return
+    // when use at onMutate -> need to return cached so then pass it up to context
+    return { cached }
+  }
+
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    // this code is responsible for update the cached name at header
-    // when update profile = header name will updated too
-    onSuccess(_, { description, name }) {
-      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-        'managed-restaurant',
-      ])
-      if (cached) {
-        queryClient.setQueryData<GetManagedRestaurantResponse>(
-          ['managed-restaurant'],
-          {
-            ...cached,
-            name,
-            description,
-          },
-        )
+    // otmist interface
+    // this code makes the same thing that commented code below
+    // the difference is that header name will updated before success,
+    // onMutate not receive data as onSuccess
+    onMutate({ description, name }) {
+      // onMuted = run when start the req
+      const { cached } = updateManagedRestaurantCache({ description, name })
+
+      // save the current cache and return it as previousProfile
+      return { previousProfile: cached }
+    },
+    // and if happens any error, will undo the name update
+    // onError = run when end the req with error
+    // receive (error, variables, context)
+    // first param -> error = error details
+    // second param -> variables = the data that we send on req
+    // third param -> conext = info that we can share between different parts of a mutation/query
+    // the context will nest everything that will be returned from mutation/query methods
+    // so when i return { previousProfile: cached } in onMutate, this automaticaly turns onError be able to see/work with that data through the context
+    onError(_, __, context) {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile)
       }
     },
+    // you can use onSuccess (line 26 - 41) instead of onMutate + onError
+    // onSuccess(_, { description, name }) { ... }// onSuccess = run when end the req with success
   })
 
   async function handleUpdateProfile(data: StoreProfileSchema) {
