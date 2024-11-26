37df48d8f805f85c2fbab73f0bd2d8c2dd8a0597
jemluz <contatojemima@gmail.com>
2024-11-26 10:16:12 -0300

    fix: intelisence / types around spec files

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    setupFiles: ['./test/setup.ts'],
     environment: 'happy-dom',
+    setupFiles: ['./src/setupTests.ts'],
   },
 } as UserConfig & {
   test: InlineConfig
