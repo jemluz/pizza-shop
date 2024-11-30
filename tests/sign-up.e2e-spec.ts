3db753b252e4875ba63cc4da8653e5f0f1169472
jemluz <contatojemima@gmail.com>
2024-11-30 20:45:51 -0300

    fix: replace waitForTimeout( ) by await

--- tests/sign-up.e2e-spec.ts
+++ tests/sign-up.e2e-spec.ts
@@ -11,7 +11,7 @@ test('sign up successfully', async ({ page }) => {
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
   const toast = page.getByText('Restaurante cadastrado com sucesso')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('sign up with error', async ({ page }) => {
@@ -25,7 +25,7 @@ test('sign up with error', async ({ page }) => {
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
   const toast = page.getByText('Erro ao cadastrar restaurante')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to login page', async ({ page }) => {
