40805eddcde400a32a4d29df6475bd63452f07e1
jemluz <contatojemima@gmail.com>
2024-11-30 14:07:17 -0300

    feat: add sign in mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,11 +2,13 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { signInMock } from './sign-in.mock'
+
 // only when enableMSW is called, MSW will replace API for intercept the requests
 // and that bahavior (call enableMSW) only will happen when tests are running
 
 // need to add mode variable and it should be one of test, production or developmt values
-export const worker = setupWorker()
+export const worker = setupWorker(signInMock)
 
 export async function enableMSW() {
   // if mode = test, nothing happens
