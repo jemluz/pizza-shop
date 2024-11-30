c4a2dd5bd0921690d263d8ae131816d4838aafb7
jemluz <contatojemima@gmail.com>
2024-11-30 09:29:48 -0300

    feat: improve pagination test

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -7,21 +7,53 @@ import { Pagination } from './pagination'
 const onPageChangeCallback = vi.fn()
 
 describe('Pagination', () => {
-  it('should display the right amount of pages and results', () => {
-    const wrapper = render(
-      <Pagination
-        pageIndex={0}
-        totalCount={200}
-        perPage={10}
-        // eslint-disable-next-line prettier/prettier
-        onPageChange={() => { }}
-      />,
-    )
-    expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
-    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  beforeEach(() => {
+    // if onPageChangeCallback is a global spie, the test results could be
+    // corrupted, bcs once is called in one test, other tests can verify as called even before call it again
+
+    // so in this case we need to reset the spie "data/memory" between each test with mockClear()
+    onPageChangeCallback.mockClear()
   })
 
-  it('should be able to navigate to the next page', async () => {
+  it('should be able to navigate to the previous page', async () => {
+    const user = userEvent.setup()
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior',
+    })
+
+    await user.click(nextPageButton)
+    expect(onPageChangeCallback).toHaveBeenCalledWith(4)
+  })
+
+  it('should be able to navigate to the first page', async () => {
+    const user = userEvent.setup()
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Primeira página',
+    })
+
+    await user.click(nextPageButton)
+    expect(onPageChangeCallback).toHaveBeenCalledWith(0)
+  })
+
+  it('should be able to navigate to the last page', async () => {
     const user = userEvent.setup()
     const wrapper = render(
       <Pagination
@@ -31,10 +63,12 @@ describe('Pagination', () => {
         onPageChange={onPageChangeCallback}
       />,
     )
+
     const nextPageButton = wrapper.getByRole('button', {
-      name: 'Próxima página',
+      name: 'Última página',
     })
+
     await user.click(nextPageButton)
-    expect(onPageChangeCallback).toHaveBeenCalledWith(1)
+    expect(onPageChangeCallback).toHaveBeenCalledWith(19)
   })
 })
