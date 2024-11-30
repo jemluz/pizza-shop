fd9b644b319113aa15eb72abb694283673880591
jemluz <contatojemima@gmail.com>
2024-11-30 16:05:15 -0300

    feat: add dashboard charts and cards mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,13 +2,29 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period.mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount.mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount.mock'
+import { getMonthRevenueMock } from './get-month-revenue.mock'
+import { getPopularProductsMock } from './get-popular-products.mock'
+import { registerRestaurantMock } from './register-restaurant.mock'
 import { signInMock } from './sign-in.mock'
+
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 // only when enableMSW is called, MSW will replace API for intercept the requests
 // and that bahavior (call enableMSW) only will happen when tests are running
 
 // need to add mode variable and it should be one of test, production or developmt values
-export const worker = setupWorker(signInMock)
 
 export async function enableMSW() {
   // if mode = test, nothing happens
